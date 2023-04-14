66901512e45e619eb63980545a74c2c2c7e655f5
Klemens Vospernik <klemens.vospernik@gmx.at>
2023-04-14 12:31:32 +0200

    changed CrawlingManager and Main to corrected types, deleted CrawlingList.java, working display of headers

--- src/main/java/CrawlingList.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class CrawlingList<Website> {
-    // data structure
-    /*
-    public ArrayList<String> headings;
-    public ArrayList<String> functionalLinks;
-    public ArrayList<String> brokenLinks; */
-}
