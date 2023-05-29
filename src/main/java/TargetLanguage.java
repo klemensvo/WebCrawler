f670c458bbbd9d04b06f120fb40800b444a26c8d
OlgaKupar <Oljalya.topolja@gmail.com>
2023-05-29 14:26:08 +0200

    Improved TargetLanguage to interact with DeeplAPIWrapper

--- src/main/java/TargetLanguage.java
+++ src/main/java/TargetLanguage.java
@@ -1,37 +1,66 @@
+import com.deepl.api.Language;
+
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class TargetLanguage {
     String targetLanguage;
-    DeeplAPIWrapper deeplAPIWrapper = new DeeplAPIWrapper();
+
 
     String getTargetLanguageFromUser() {
         String targetLanguage = "";
         do {
             printPromptForTargetLanguage();
-            targetLanguage = getUserInputLanguage();
+            String userInput = getUserInputLanguage();   //original user input
+            String formatedUserInput = getFormattedInputLanguage(userInput); //brings the string f.e. "CHInESE" to form "Chinese"
+            String normalizedLanguageVariant = getNationalLanguageFormat(formatedUserInput);// converts to an API specific form f.e. "Chinese (simplified)"
+            targetLanguage = normalizedLanguageVariant;
 
         } while (!isValidTargetLanguage());
         return targetLanguage;
     }
 
-    //todo: add functionality to check if the chosen API support the user defined target language
+    //todo: add functionality to check if the chosen API supports the user defined target language
     protected boolean isValidTargetLanguage() {
-        //ArrayList<String> validLanguages = deeplAPIWrapper.getLanguageName();
-        //if(!validLanguages.contains(targetLanguage)){
-            //return false;
-       // }
+        DeeplAPIWrapper deeplAPIWrapper = new DeeplAPIWrapper();
+        List<Language> supportedAPILanguages = deeplAPIWrapper.getLanguages();
+        ArrayList <String> supportedLanguages = deeplAPIWrapper.getLanguageNamesList(supportedAPILanguages);
+
+        if(!supportedLanguages.contains(targetLanguage)){
+            return false;
+        }
         return true;
     }
 
     private void printPromptForTargetLanguage(){
-        System.out.print("Please enter a target language (for example \"English\"): ");
+        System.out.print("Please enter a target language : ");
     }
 
     private String getUserInputLanguage(){      //example output "English"
         Scanner scanner = new Scanner(System.in);
         String userInputLanguage = scanner.nextLine();
         return userInputLanguage;
+    }
+
+    private String getFormattedInputLanguage (String language){
+        String formattedInputLanguage = language.substring(0,1).toUpperCase()+language.substring(1).toLowerCase();
+        return formattedInputLanguage;
+    }
+
+    //convertion is required by Deepl API
+    private String getNationalLanguageFormat (String inputLanguageString){
+        String formattedLanguageString = "";
+        if(inputLanguageString.equals("English")){
+            formattedLanguageString = "English "+"(British)";
+        }else if(inputLanguageString.equals("Portuguese")){
+            formattedLanguageString = "Portuguese "+"(European)";
+        }else if(inputLanguageString.equals("Chinese")){
+            formattedLanguageString = "Chinese "+"(simplified)";
+        }else{
+            formattedLanguageString = inputLanguageString;
+        }
+        return formattedLanguageString;
     }
 
 
